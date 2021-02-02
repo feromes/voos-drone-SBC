0db2d16a131b12d76ab67e2c83d5a8adc500c874
Fernando Gomes <feromes@gmail.com>
2021-02-02 11:56:15 -0300

    Adicionando alguns bairros

---
+++ balnearia/data/BairrosInternos_1.js
@@ -0,0 +1,5 @@
+var json_BairrosInternos_1 = {"type":"FeatureCollection","name":"BairrosInternos_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[
+    { "type": "Feature", "properties": { "id": null, "BAIRRO": "VILA BALNEÁRIA" }, "geometry": { "type": "Point", "coordinates": [ -46.542010176974294, -23.767919510358851 ] } },
+    { "type": "Feature", "properties": { "id": null, "BAIRRO": "VISTA ALEGRE" }, "geometry": { "type": "Point", "coordinates": [ -46.552663708357912, -23.776878902195236 ] } }
+    ]
+    }
