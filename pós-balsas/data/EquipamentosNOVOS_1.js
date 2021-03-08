2027757b93dc09a0fabb2d7e961c7fe6704549f7
Fernando Gomes <feromes@gmail.com>
2021-03-08 12:03:24 -0300

    Atualizando pós balsas

---
+++ pós-balsas/data/EquipamentosNOVOS_1.js
@@ -0,0 +1,9 @@
+var json_EquipamentosNOVOS_1 = {
+"type": "FeatureCollection",
+"name": "EquipamentosNOVOS_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NOME": "Casa de triagem - Estrada do Rio Acima ", "Video_0": "videos\/incluidos\/videos\/pos-balsa\/Casa de triagem - Estrada do Rio Acima.MP4", "TIPO": "COLETA SELETIVA" }, "geometry": { "type": "Point", "coordinates": [ -46.598586764494591, -23.844554304192179 ] } },
+{ "type": "Feature", "properties": { "NOME": "NOVA UBS SANTA CRUZ", "Video_0": "videos\/incluidos\/videos\/pos-balsa\/NOVA UBS SANTA CRUZ.MP4", "TIPO": "SAÚDE" }, "geometry": { "type": "Point", "coordinates": [ -46.596899429385822, -23.843054748962821 ] } }
+]
+}
